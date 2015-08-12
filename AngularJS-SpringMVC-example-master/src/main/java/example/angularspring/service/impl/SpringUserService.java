09399e0f4ee513370d40680f253356fe4652700a
juliolopezlosada <julitolo@hotmail.com>
2015-08-12 11:01:40 +0200

    second change test

--- AngularJS-SpringMVC-example-master/src/main/java/example/angularspring/service/impl/SpringUserService.java
+++ AngularJS-SpringMVC-example-master/src/main/java/example/angularspring/service/impl/SpringUserService.java
@@ -1,18 +1,12 @@
 package example.angularspring.service.impl;
 
-import example.angularspring.dto.Person;
-import example.angularspring.dto.User;
-import example.angularspring.service.PersonService;
-import example.angularspring.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import example.angularspring.dto.User;
+import example.angularspring.service.UserService;
 
 /**
  * UserService that accesses the spring credentials.
