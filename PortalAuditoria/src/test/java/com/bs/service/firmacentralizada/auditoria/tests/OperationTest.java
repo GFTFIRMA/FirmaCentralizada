5b206541e2a1231861833cc81d612408ebe5ff48
juliolopezlosada <julitolo@hotmail.com>
2015-08-12 17:06:19 +0200

    Unit tests

--- PortalAuditoria/src/test/java/com/bs/service/firmacentralizada/auditoria/tests/OperationTest.java
+++ PortalAuditoria/src/test/java/com/bs/service/firmacentralizada/auditoria/tests/OperationTest.java
@@ -2,6 +2,8 @@ package com.bs.service.firmacentralizada.auditoria.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -9,87 +11,129 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.bs.service.firmacentralizada.auditoria.AuditoriaConfig;
 import com.bs.service.firmacentralizada.auditoria.dao.AuditoriaDAO;
+import com.bs.service.firmacentralizada.auditoria.dao.OperationDataFilter;
+import com.bs.service.firmacentralizada.auditoria.entidades.Operation;
+import com.bs.service.firmacentralizada.auditoria.entidades.OperationActivity;
 import com.bs.service.firmacentralizada.auditoria.entidades.OperationData;
 
 public class OperationTest {
 	
-	private void changeBoolean(Boolean b) {
-		b ^= b;
-	}
-	
 	@Test
 	public void testSelectOperationActivity() {
 		
-		Boolean b = false;
-		System.out.println(b);
-		changeBoolean(b);
-		System.out.println(b);
-		changeBoolean(b);
-		System.out.println(b);
-		changeBoolean(b);
-		System.out.println(b);
-		
-		
-//		
-//		AnnotationConfigApplicationContext context =
-//			    new AnnotationConfigApplicationContext(AuditoriaConfig.class);
-//		AuditoriaDAO auditoriaDao = context.getBean(AuditoriaDAO.class);
-//		List<OperationActivity> result = auditoriaDao.getOperationActivity(4, 1);
-//		
-//		System.out.println(" -- Número de entradas: " + result.size());
-//		
-//		for (int index = 0; index < result.size(); index++) {
-//			System.out.println();
-//			System.out.println("  --- " + (index + 1));
-//			System.out.println("  ---- ACTIVITY_ID: " + result.get(index).getActivityId());
-//			System.out.println("  ---- OPERATION_ID: " + result.get(index).getOperation().getOperationPK().getOperationId());
-//			System.out.println("  ---- ITERATION: " + result.get(index).getOperation().getOperationPK().getIteration());
-//			System.out.println("  ---- STEP: " + result.get(index).getStepId());
-//			System.out.println("  ---- LAYER: " + result.get(index).getLayer().getLayerId());
-//			System.out.println("  ---- COMPONENT: " + result.get(index).getComponentService().getComponentServicesPK().getComponent().getComponent());
-//			System.out.println("  ---- SERVICE: " + result.get(index).getComponentService().getComponentServicesPK().getService());
-//			System.out.println("  ---- EXECUTION_POINT: " + result.get(index).getExecutionPoint().getExecutionPoint());
-//			System.out.println("  ---- START: " + result.get(index).getStartTime());
-//			System.out.println("  ---- END: " + result.get(index).getEndTime());
-//			System.out.println("  ---- TRACKING: " + result.get(index).getTrackingId());
-//			System.out.println("  ---- REQUEST: " + result.get(index).getXmlSvcRequest());
-//			System.out.println("  ---- RESPONSE: " + result.get(index).getXmlSvcResponse());
-//			System.out.println("  ---- CONTEXT: " + result.get(index).getXmlSvcContext());
-//			System.out.println("  ---- STATUS: " + result.get(index).getStatus().getStatusId());
-//			System.out.println("  ---- RETURN CODE: " + result.get(index).getReturnCode());
-//			System.out.println("  ---- RETURN DESCRIPTION: " + result.get(index).getReturnDescription());
-//			System.out.println("  ---- NODE: " + result.get(index).getNode());
-//		}
-		
-		assertTrue(b);
-	}
-	
-	//@Test
-	public void testSelectAllOperationData() {
 		AnnotationConfigApplicationContext context =
 			    new AnnotationConfigApplicationContext(AuditoriaConfig.class);
 		AuditoriaDAO auditoriaDao = context.getBean(AuditoriaDAO.class);
+		
+		List<OperationActivity> result = auditoriaDao.getOperationActivity(4, 1);
+		
+		System.out.println(" -- Número de entradas: " + result.size());
+		for (int i = 0; i < result.size(); i++) 
+			printOperationActivity(result.get(i));
+		
+		assertFalse(result.isEmpty());
+		context.close();
+	}
+	
+	@Test
+	public void testSelectAllOperationData() {
+		
+		AnnotationConfigApplicationContext context =
+			    new AnnotationConfigApplicationContext(AuditoriaConfig.class);
+		AuditoriaDAO auditoriaDao = context.getBean(AuditoriaDAO.class);
+		
 		List<OperationData> components = auditoriaDao.getOperationData(null);
-		printFormat(components);
+		
+		System.out.println(" -- Número de entradas: " + components.size());
+		for (int i = 0; i < components.size(); i++) 
+			printOperationData(components.get(i));
+		
 		assertFalse(components.isEmpty());
 		context.close();
 	}
 	
-	private void printFormat(List<OperationData> components) {
-		System.out.println(" -- Número de entradas: " + components.size());
+	@Test
+	public void testSelectFilteredOperationData() {
 		
-		for (int index = 0; index < components.size(); index++) {
-			System.out.println();
-			System.out.println("  --- " + (index + 1));
-			System.out.println("  ---- OPERATION_ID: " + components.get(index).getOperation().getOperationPK().getOperationId());
-			System.out.println("  ---- ITERATION: " + components.get(index).getOperation().getOperationPK().getIteration());
-			System.out.println("  ---- NODO: " + components.get(index).getOperation().getNode());
-			System.out.println("  ---- RESULT_CODE: " + components.get(index).getOperation().getResultCode().getCode());
-			System.out.println("  ---- FLOW: " + components.get(index).getFlow());
-			System.out.println("  ---- REQUEST_ID: " + components.get(index).getRequestId());
-			System.out.println("  ---- FC_ID: " + components.get(index).getFcId());
-			System.out.println("  ---- SESSION_ID: " + components.get(index).getSessionId());
-		}
+		AnnotationConfigApplicationContext context =
+			    new AnnotationConfigApplicationContext(AuditoriaConfig.class);
+		AuditoriaDAO auditoriaDao = context.getBean(AuditoriaDAO.class);
+		
+		OperationDataFilter odf = new OperationDataFilter();
+		Calendar d = Calendar.getInstance();
+		odf.setOperationId(4);
+		odf.setFlow("20");
+		odf.setSessionId("sessionabcdef");
+		odf.setFcId("fcidabcdef");
+		odf.setStatusId(12);
+		d.set(2015, 06, 12, 14, 34, 00);
+		odf.setStartTime(new Date(d.getTimeInMillis()));
+		d.set(2015, 06, 12, 15, 59, 59);
+		odf.setEndTime(new Date(d.getTimeInMillis()));
+		odf.setInputChannel("inputChannel1");
+		
+		List<OperationData> components = auditoriaDao.getOperationData(odf);
+		
+		System.out.println(" -- Número de entradas: " + components.size());
+		for (int i = 0; i < components.size(); i++) 
+			printOperationData(components.get(i));
+		
+		assertFalse(components.isEmpty());
+		context.close();
 	}
-
+	
+	@Test
+	public void testSelectNumIterations() {
+		
+		AnnotationConfigApplicationContext context =
+			    new AnnotationConfigApplicationContext(AuditoriaConfig.class);
+		AuditoriaDAO auditoriaDao = context.getBean(AuditoriaDAO.class);
+		
+		Integer i = auditoriaDao.getTotalIterations(4);
+		
+		System.out.println(" -- Número de iteraciones: " + i);
+		
+		assertTrue(i == 3);
+		context.close();
+	}
+	
+	//** UTILIDADES **//
+	
+	private void printOperation(Operation o) {
+		System.out.println("  ---- OPERATION ID: " + o.getOperationPK().getOperationId());
+		System.out.println("  ---- ITERATION: " + o.getOperationPK().getIteration());
+		System.out.println("  ---- OPERATION START TIME: " + o.getStartTime());
+		System.out.println("  ---- OPERATION END TIME: " + o.getEndTime());
+		System.out.println("  ---- RESULT CODE: " + o.getResultCode().getCode());
+		System.out.println("  ---- OPERATION NODE: " + o.getNode());
+		System.out.println("  ---- OPERATION STATUS ID: " + o.getStatusId().getStatusId());
+	}
+	
+	private void printOperationData(OperationData o) {
+		printOperation(o.getOperation());
+		System.out.println("  ---- FLOW: " + o.getFlow().getFlow());
+		System.out.println("  ---- INPUT_CHANNEL: " + o.getInputChannel());
+		System.out.println("  ---- FC_ID: " + o.getFcId());
+		System.out.println("  ---- SESSION_ID: " + o.getSessionId());
+		System.out.println("  ---- REQUEST: " + o.getXmlRequest());
+		System.out.println("  ---- RESPONSE: " + o.getXmlResponse());
+	}
+	
+	private void printOperationActivity(OperationActivity o) {
+		System.out.println("  ---- ACTIVITY ID: " + o.getActivityId());
+		printOperation(o.getOperation());
+		System.out.println("  ---- LAYER ID: " + o.getLayer().getLayerId());
+		System.out.println("  ---- COMPONENT: " + o.getComponentService().getComponentServicesPK().getComponent().getComponent());
+		System.out.println("  ---- SERVICE: " + o.getComponentService().getComponentServicesPK().getService());
+		System.out.println("  ---- EXECUTION POINT: " + o.getExecutionPoint().getExecutionPoint());
+		System.out.println("  ---- ACTIVITY START TIME: " + o.getStartTime());
+		System.out.println("  ---- ACTIVITY END TIME: " + o.getEndTime());
+		System.out.println("  ---- TRACKING ID: " + o.getTrackingId());
+		System.out.println("  ---- ACTIVITY NODE: " + o.getNode());
+		System.out.println("  ---- STATUS ID: " + o.getStatus().getStatusId());
+		System.out.println("  ---- RETURN CODE: " + o.getReturnCode());
+		System.out.println("  ---- RETURN DESCRIPTION: " + o.getReturnDescription());
+		System.out.println("  ---- REQUEST: " + o.getXmlSvcRequest());
+		System.out.println("  ---- RESPONSE: " + o.getXmlSvcResponse());
+	}
 }
